ce8c539bc6687c79a0a1638493c6bd4ac0ec74ca
Félix David Mejía Mejía <fmejia01@gmail.com>
2015-07-25 16:34:31 -0400

    Only comment changes. Updated comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,14 @@
 ## These functions create an special matrix object (1) which is able to
 ## cache its inverse matrix (2)
 
-## Function makeCacheMatrix: This is a constructor for a special matrix object
+## Function makeCacheMatrix (1): This is a constructor for a special matrix object
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## This function caches the object inverse matrix, so, if it has been already calculated, it doesn't calc it again
+## Function cacheSolve (2): This function caches the object inverse matrix, so, if
+## it has been already calculated, it does not calc it again
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
